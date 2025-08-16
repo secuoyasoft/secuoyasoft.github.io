cb28055266422a65d0294ce21c9eae1a949ffcff
andres rodriguez <andresdrc22@gmail.com>
2025-08-16 00:56:54 -0400

    correcion 3

--- elecciones2025/flutter_bootstrap.js
+++ elecciones2025/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "311070095"
+    serviceWorkerVersion: "1939019964"
   }
 });

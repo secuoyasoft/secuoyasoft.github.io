cd7cf541f78db8cbe2abcf8c7163660965e6afa1
andres rodriguez <andresdrc22@gmail.com>
2025-08-19 20:32:08 -0400

    correcion 6

--- elecciones2025/flutter_service_worker.js
+++ elecciones2025/flutter_service_worker.js
@@ -57,7 +57,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6db092739a03171f4eb9f83815a393d4
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "5c76f06214e626194cac98258a99ba74",
+"flutter_bootstrap.js": "a82d312d7b44c47ee25e5e8d059854d2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
